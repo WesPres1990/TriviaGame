275bbeceb12997216b7b2db6e5948613647b313e
WesPres1990 <lawpres@gmail.com>
2017-12-28 23:38:51 -0500

    Sixth Upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,44 @@
-
+// Timer
 var timeInSecs;
 var ticker;
 
 function startTimer(secs){
 timeInSecs = parseInt(secs)-1;
-ticker = setInterval("tick()",1000);   // every second
+// Every second
+ticker = setInterval("tick()",1000);
 }
-
-function stop(){
-    // Clears our "counter" interval. The interval name is passed to the clearInterval function.
-        clearInterval(ticker);
-    }
-;
 
 function tick() {
 var secs = timeInSecs;
 if (secs>0) {
 timeInSecs--;
 } else {
+    // Stop counting at zero and show results
 clearInterval(ticker);
-stop();
-showResults(); // stop counting at zero and show results
-// startTimer(30);  // remove forward slashes in front of startTimer to repeat if required
+$('.jumbotron-3').show();
+$('.jumbotron-2').hide();
+showResults();
+// Removing forward slashes in front of startTimer supposed repeat if required, but does not work
+// startTimer(30);
 }
+
+// Displaying timer
 
 //*document.getElementById("countdown").innerHTML = "Time Remaining: " + secs;*//
 $('#countdown').html('<h2>' + "Time Remaining: " + secs +'</h2>');
 }
 
-//startTimer(30);  // 30 seconds
-
-
+// Building the quiz
 function buildQuiz() {
-      // we'll need a place to store the HTML output
+      // Store the HTML output
       var output = [];
   
-      // for each question...
+      // For each question...
       myQuestions.forEach((currentQuestion, questionNumber) => {
         // we'll want to store the list of answer choices
         var answers = [];
   
-        // and for each available answer...
+        // And for each available answer...
         for (letter in currentQuestion.answers) {
           // ...add an HTML radio button
           answers.push(
@@ -52,27 +50,27 @@ function buildQuiz() {
           );
         }
   
-        // add this question and its answers to the output
+        // Add this question and its answers to the output
         output.push(
           `<div class="question"><h3> ${currentQuestion.question} </h3></div>
           <div class="answers"> ${answers.join("")} </div>`
         );
       });
   
-      // finally combine our output list into one string of HTML and put it on the page
+      // Combine output list into one string of HTML and put it on the page
       quizContainer.innerHTML = output.join("<h2></h2>");
     }
   
     function showResults() {
-      // gather answer containers from our quiz
+      // Gather answer containers from quiz
       var answerContainers = quizContainer.querySelectorAll(".answers");
   
-      // keep track of user's answers
-      let numCorrect = 0;
-      let numIncorrect = 0;
-      let numUnanswered = 0;
+      // Keep track of user answers
+      var numCorrect = 0;
+      var numIncorrect = 0;
+      var numUnanswered = 0;
   
-      // for each question...
+      // For each question...
       myQuestions.forEach((currentQuestion, questionNumber) => {
         // find selected answer
         var answerContainer = answerContainers[questionNumber];
@@ -99,14 +97,15 @@ function buildQuiz() {
         }
       });
   
-      // show results
+      // Show results
       $('#results').html('Correct Answers: '+numCorrect+ "<br>" +'Incorrect Answers: '+numIncorrect+ "<br>" +'Unanswered: '+numUnanswered);
     }
+
+// Variables giving reference to HTML elements; includes questions and answers array
   
     var quizContainer = document.getElementById("quiz");
     var resultsContainer = document.getElementById("results");
     var submitButton = document.getElementById("submit");
-    var startButton = document.getElementById("startgame");
     var myQuestions = [
       {
         question: "What was the name of Titanic's older sister ship?",
@@ -162,65 +161,33 @@ function buildQuiz() {
       }
     ];
 
+//Starting the game and jumbontron controls
+
     // This initializes the button that starts the game 
     $(".jumbotron-1").on("click", function (){
-        // when the start button clicked, the div with the questions that was hidden is shown
+        // When the "Start" button is clicked, the div with the questions that was hidden is shown and the timer begins to run
                 $('.jumbotron-2').show();
                 startTimer(30);
-                console.log('hello');
-        
                 $(this).hide();
     });
 
-    // This initializes the button that starts the game 
+    // This allows the user to click the "Done" button and end the game 
     $("#submit").on("click", function (){
-        // when the start button clicked, the div with the questions that was hidden is shown
+        // when the "Done" button is clicked, the div with the results that was hidden is shown
                 $('.jumbotron-3').show();
-                console.log('hello');
-            
                 $('.jumbotron-2').hide();
     });
-
+    
+    // This was for allowing the user to restart the game, but I could not get it to work properly
     //$(".jumbotron-3").on("click", function (){
-    // when the start button clicked, the div with the questions that was hidden is shown
-            //$('.jumbotron-1').show();
-            //console.log('hello');
-            
+    // when the "Restart" button is clicked, the div with the opening jumbotron that was hidden is shown
+            //$('.jumbotron-1').show(); 
             //$(this).hide();
     //});
   
-    // display quiz right away
+    // Display quiz right away
     buildQuiz();
   
-    // on submit, show results
+    // On submit, show results
     submitButton.addEventListener("click", showResults);
-
-    //countdownContainer.addEventListener("click", showResults);
-
-    //submitButton.addEventListener("click", clearInterval);
-
-    //startButton.addEventListener("click", tick);
 ;
-
-//*var guessedAnswer = 0*//
-
-//*var correctAnswer = 0
-
-//*var questions = ["What was the name of Titanic's older sister ship?", "Who founded New York in 1609?", "Which one of these presidents is not typically ranked in the top three?", "How does Harry Potter hide from his enemies?"];
-
-
-
-
-//*function gameFunction (){
-    //*for (var i = 0; i<questions.length; i++) {
-		//*if (guessedAnswer===correctAnswer)) {
-			//*correct++;
-		//*} else if (guessedAnswer===wrongAnswers) {
-            //incorrect++;
-			//*} else {
-				//*unAnswered++;
-			//*}
-		//*}
-    //*}
-    //*$('.results').html('correct: '+correct+ "<br>" +'incorrect: '+incorrect+ "<br>" +'unanswered: '+unAnswered);
-//*}*//
